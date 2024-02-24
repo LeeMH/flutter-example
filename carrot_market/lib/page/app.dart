3d84408d49693f29ebc49c661f327fb596adfa6b
mhlee <lee.munhaeng@gmail.com>
2024-02-24 18:08:55 +0900

    page 분리

--- carrot_market/lib/page/app.dart
+++ carrot_market/lib/page/app.dart
@@ -1,3 +1,4 @@
+import 'package:carrot_market/page/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +14,6 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<Map<String, String>> datas = [];
   int _currentPageIndex = 0;
 
   @override
@@ -21,214 +21,50 @@ class _AppState extends State<App> {
     super.initState();
 
     _currentPageIndex = 0;
-    datas = [
-      {
-        "image": "assets/images/ara-1.jpg",
-        "title": "네메시스 축구화275",
-        "location": "제주 제주시 아라동",
-        "price": "30000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/ara-2.jpg",
-        "title": "LA갈비 5kg팔아요~",
-        "location": "제주 제주시 아라동",
-        "price": "100000",
-        "likes": "5"
-      },
-      {
-        "image": "assets/images/ara-3.jpg",
-        "title": "치약팝니다",
-        "location": "제주 제주시 아라동",
-        "price": "5000",
-        "likes": "0"
-      },
-      {
-        "image": "assets/images/ara-4.jpg",
-        "title": "[풀박스]맥북프로16인치 터치바 스페이스그레이",
-        "location": "제주 제주시 아라동",
-        "price": "2500000",
-        "likes": "6"
-      },
-      {
-        "image": "assets/images/ara-5.jpg",
-        "title": "디월트존기임팩",
-        "location": "제주 제주시 아라동",
-        "price": "150000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/ara-6.jpg",
-        "title": "갤럭시s10",
-        "location": "제주 제주시 아라동",
-        "price": "180000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/ara-7.jpg",
-        "title": "선반",
-        "location": "제주 제주시 아라동",
-        "price": "15000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/ara-8.jpg",
-        "title": "냉장 쇼케이스",
-        "location": "제주 제주시 아라동",
-        "price": "80000",
-        "likes": "3"
-      },
-      {
-        "image": "assets/images/ara-9.jpg",
-        "title": "대우 미니냉장고",
-        "location": "제주 제주시 아라동",
-        "price": "30000",
-        "likes": "3"
-      },
-      {
-        "image": "assets/images/ara-10.jpg",
-        "title": "멜킨스 풀업 턱걸이 판매합니다.",
-        "location": "제주 제주시 아라동",
-        "price": "50000",
-        "likes": "7"
-      },
-    ];
   }
 
-  PreferredSizeWidget _appBar() {
-    return AppBar(
-      elevation: 1,
-      title: GestureDetector(
-        onTap: () {
-          if (kDebugMode) {
-            print('클릭');
-          }
-        },
-        child: const Row(
-          children: [
-            Text('아라동'),
-            Icon(Icons.arrow_drop_down),
-          ],
-        ),
-      ),
-      actions: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.search),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.tune),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: SvgPicture.asset(
-            "assets/svg/bell.svg",
-            width: 22,
-          ),
-        ),
-      ],
-    );
-  }
-
-  final oCcy = NumberFormat("#,###", "ko_KR");
-  String currencyFormat(String price) {
-    return '${oCcy.format(int.parse(price))}원';
-  }
-
-  Widget _bodyWidget() {
-    return ListView.separated(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 10,
-      ),
-      itemBuilder: (BuildContext context, int index) {
+  Widget _buildBodyWidget(int index) {
+    switch (index) {
+      case 0:
+        return const HomeWidget();
+      case 1:
         return Container(
-          padding: const EdgeInsets.symmetric(
-            vertical: 10,
-          ),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(10)),
-                child: Image.asset(
-                  datas[index]["image"]!,
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                child: Container(
-                  height: 100,
-                  padding: const EdgeInsets.only(
-                    left: 10,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        datas[index]["title"]!,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                          fontSize: 15,
-                        ),
-                      ),
-                      Text(
-                        datas[index]["location"]!,
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.black.withOpacity(0.3),
-                        ),
-                      ),
-                      Text(
-                        currencyFormat(datas[index]["price"]!),
-                        style: TextStyle(fontWeight: FontWeight.w500),
-                      ),
-                      Expanded(
-                        child: Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              SvgPicture.asset(
-                                "assets/svg/heart_off.svg",
-                                width: 13,
-                                height: 13,
-                              ),
-                              const SizedBox(
-                                width: 5,
-                              ),
-                              Text(datas[index]["likes"]!),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+          child: const Center(
+            child: Text('동네생활'),
           ),
         );
-      },
-      separatorBuilder: (BuildContext context, int index) {
+      case 2:
         return Container(
-          height: 1,
-          color: Colors.black.withOpacity(0.5),
+          child: const Center(
+            child: Text('내근처'),
+          ),
         );
-      },
-      itemCount: 10,
-    );
+      case 3:
+        return Container(
+          child: const Center(
+            child: Text('채팅'),
+          ),
+        );
+      case 4:
+        return Container(
+          child: const Center(
+            child: Text('나의당근'),
+          ),
+        );
+      default:
+        return Container();
+    }
   }
 
   BottomNavigationBarItem _bottomNavigationBarItem(
       String iconName, String label) {
     return BottomNavigationBarItem(
       icon: SvgPicture.asset(
-        "assets/svg/$iconName.svg",
+        "assets/svg/${iconName}_off.svg",
+        width: 22,
+      ),
+      activeIcon: SvgPicture.asset(
+        "assets/svg/${iconName}_on.svg",
         width: 22,
       ),
       label: label,
@@ -238,10 +74,10 @@ class _AppState extends State<App> {
   Widget _bottomNavigationBar() {
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
+      currentIndex: _currentPageIndex,
       selectedItemColor: Colors.black,
       selectedLabelStyle: const TextStyle(
         color: Colors.black,
-        fontWeight: FontWeight.w600,
       ),
       onTap: (int index) {
         if (kDebugMode) {
@@ -253,7 +89,7 @@ class _AppState extends State<App> {
       },
       items: [
         _bottomNavigationBarItem("home", "홈"),
-        _bottomNavigationBarItem("note", "동네생활"),
+        _bottomNavigationBarItem("notes", "동네생활"),
         _bottomNavigationBarItem("location", "내근처"),
         _bottomNavigationBarItem("chat", "채팅"),
         _bottomNavigationBarItem("user", "나의당근"),
@@ -264,8 +100,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appBar(),
-      body: _bodyWidget(),
+      body: _buildBodyWidget(_currentPageIndex),
       bottomNavigationBar: _bottomNavigationBar(),
     );
   }
